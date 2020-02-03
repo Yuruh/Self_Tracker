c4817b61f85410749522942028fc9e986e474d3d
Antoine Lempereur <antoine.lempereur@epitech.eu>
2020-02-03 15:12:39 +0100

    setup front end, setup http serveur

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Yuruh/Self_Tracker/aftg"
-
-	//	"github.com/Yuruh/Self_Tracker/aftg"
 	"github.com/Yuruh/Self_Tracker/spotify"
 	"io/ioutil"
 	"log"
@@ -30,56 +28,62 @@ func Dummy() int64 {
 
  */
 
+func processLastPlayedSong(savedPlayer* spotify.Player, tickInterval time.Duration) {
+	var delay = aftg.GetConnector().GetSrvDelay()
+	fmt.Println("srv delay:", delay)
+	spotifyPlayer, err := spotify.GetConnector().GetCurrentTrack()
+	if err != nil {
+		if err, ok := err.(*spotify.TrackError); ok {
+			if err.Code == spotify.NotPlaying {
+				println("Not currently playing")
+			} else {
+				log.Fatal(err.Error())
+			}
+		}
+	}
+	if savedPlayer.Item.Id != spotifyPlayer.Item.Id {
+		println("Track Changed")
+
+		if savedPlayer.Item.Id != "" {
+			var durationPlayedBeforeNextTrack time.Duration = tickInterval - time.Duration(spotifyPlayer.ProgressMs) * time.Millisecond
+
+			var trackEndTime = time.Now().UnixNano() / int64(time.Millisecond) - spotifyPlayer.ProgressMs
+
+			var trackBeginTime = trackEndTime -
+				savedPlayer.ProgressMs -
+				int64(durationPlayedBeforeNextTrack / time.Millisecond)
+
+			fmt.Print("Title \"", savedPlayer.Item.Name, "\" played from ", time.Unix(trackBeginTime / 1000, 0))
+			fmt.Println(" to", time.Unix(trackEndTime / 1000, 0))
+			aftg.GetConnector().AddTag(aftg.Tag{
+				TimestampBegin: trackBeginTime,
+				TimestampEnd: trackEndTime,
+				Name: savedPlayer.Item.Artists[0].Name + "_" + savedPlayer.Item.Name,
+				ProductName: savedPlayer.Item.Artists[0].Name,
+				TagName: savedPlayer.Item.Name,
+			}, delay)
+		}
+	}
+	*savedPlayer = spotifyPlayer//.Copy()
+
+	//			print("Title ", spotifyPlayer.Item.Name)
+	//			print(" from Album ", spotifyPlayer.Item.Album.Name)
+	//			print(" by Artist ", spotifyPlayer.Item.Artists[0].Name)
+	//			println(" is playing since ", spotifyPlayer.ProgressMs, " milliseconds")
+}
+
 func runTicker() {
 //	var ticker *time.Ticker = time.NewTicker(time.Minute * 3)
 	const tickInterval time.Duration = time.Second * 45
 	var ticker *time.Ticker = time.NewTicker(tickInterval)
 
 	var savedPlayer spotify.Player
+	processLastPlayedSong(&savedPlayer, tickInterval)
+
 	for {
 		select {
-		case <- ticker.C:
-			var delay = aftg.GetConnector().GetSrvDelay()
-			fmt.Println(delay)
-			spotifyPlayer, err := spotify.GetConnector().GetCurrentTrack()
-			if err != nil {
-				if err, ok := err.(*spotify.TrackError); ok {
-					if err.Code == spotify.NotPlaying {
-						println("Not currently playing")
-					} else {
-						log.Fatal(err.Error())
-					}
-				}
-			}
-			if savedPlayer.Item.Id != spotifyPlayer.Item.Id {
-				println("Track Changed")
-
-				if savedPlayer.Item.Id != "" {
-					var durationPlayedBeforeNextTrack time.Duration = tickInterval - time.Duration(spotifyPlayer.ProgressMs) * time.Millisecond
-
-					var trackEndTime = time.Now().UnixNano() / int64(time.Millisecond) - spotifyPlayer.ProgressMs
-
-					var trackBeginTime = trackEndTime -
-						savedPlayer.ProgressMs -
-						int64(durationPlayedBeforeNextTrack / time.Millisecond)
-
-					fmt.Print("Title \"", savedPlayer.Item.Name, "\" played from ", time.Unix(trackBeginTime / 1000, 0))
-					fmt.Println(" to", time.Unix(trackEndTime / 1000, 0))
-					/*aftg.GetConnector().AddTag(aftg.Tag{
-						TimestampBegin: trackBeginTime,
-						TimestampEnd: trackEndTime,
-						Name: savedPlayer.Item.Artists[0].Name + "_" + savedPlayer.Item.Name,
-						ProductName: savedPlayer.Item.Artists[0].Name,
-						TagName: savedPlayer.Item.Name,
-					}, delay)*/
-				}
-			}
-			savedPlayer = spotifyPlayer//.Copy()
-
-//			print("Title ", spotifyPlayer.Item.Name)
-//			print(" from Album ", spotifyPlayer.Item.Album.Name)
-//			print(" by Artist ", spotifyPlayer.Item.Artists[0].Name)
-//			println(" is playing since ", spotifyPlayer.ProgressMs, " milliseconds")
+		case <-ticker.C:
+			processLastPlayedSong(&savedPlayer, tickInterval)
 		}
 	}
 }
@@ -219,15 +223,25 @@ func getAftgApiSyncDelta() int64 {
 	return delta
 }
 */
+
 func main() {
-//	err := godotenv.Load()
+	//	err := godotenv.Load()
 //	if err != nil {
 //		log.Fatal("Error loading .env file")
 //	}
 
-	runTicker()
+	// runTicker()
 
-	//	println(buildSpotifyAuthUri())
+//	http.Handle("/foo", fooHandler)
+
+	http.HandleFunc("/spotify", func(w http.ResponseWriter, r *http.Request) {
+		println(buildSpotifyAuthUri())
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(buildSpotifyAuthUri()))
+	})
+
+	log.Fatal(http.ListenAndServe(":8090", nil))
+
 
 	//var access = getAccessFromRefresh()
 	//getCurrentTrack(access.AccessToken)
